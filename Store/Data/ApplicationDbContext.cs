97473878e6721dd9aaf86ff210b953b545909dcc
IgorSS <igorskor0912@gmail.com>
2024-02-12 01:40:00 +0200

    Create CategoryTable

--- Store/Data/ApplicationDbContext.cs
+++ Store/Data/ApplicationDbContext.cs
@@ -8,4 +8,6 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
     }
+
+    public DbSet<Category> Categories { get; set; }
 }
