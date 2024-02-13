c8b71d58f413843e6679c208b6826bcd9f910dda
IgorSS <igorskor0912@gmail.com>
2024-02-13 18:11:52 +0200

    Add validations for "Add Category" page

--- Store/Controllers/CategoryController.cs
+++ Store/Controllers/CategoryController.cs
@@ -7,10 +7,12 @@ namespace Store.Controllers;
 public class CategoryController : Controller
 {
     private readonly ApplicationDbContext _db;
+
     public CategoryController(ApplicationDbContext db)
     {
         _db = db;
     }
+
     public IActionResult Index()
     {
         List<Category> objCategoryList = _db.Categories.ToList();
@@ -25,8 +27,12 @@ public class CategoryController : Controller
     [HttpPost]
     public IActionResult Create(Category obj)
     {
-        _db.Categories.Add(obj);
-        _db.SaveChanges();
-        return RedirectToAction("Index");
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View();
     }
 }
