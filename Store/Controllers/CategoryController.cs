9bc7daf7cc83490c03c7eddf433bcd83816b3733
IgorSS <igorskor0912@gmail.com>
2024-02-13 21:15:19 +0200

    Add Edit Category functionality

--- Store/Controllers/CategoryController.cs
+++ Store/Controllers/CategoryController.cs
@@ -35,4 +35,33 @@ public class CategoryController : Controller
         }
         return View();
     }
+
+    public IActionResult Edit(int? id)
+    {
+        if (id is null or 0)
+        {
+            return NotFound();
+        }
+
+        Category? categoryFromDb = _db.Categories.Find(id);
+
+        if (categoryFromDb == null)
+        {
+            return NotFound();
+        }
+
+        return View(categoryFromDb);
+    }
+    
+    [HttpPost]
+    public IActionResult Edit(Category obj)
+    {
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
 }
