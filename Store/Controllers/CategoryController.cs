08a7fcefbdc9f5f3fdbf2abad9aeda2af639c29b
IgorSS <igorskor0912@gmail.com>
2024-02-13 22:47:48 +0200

    Add toastr notifications

--- Store/Controllers/CategoryController.cs
+++ Store/Controllers/CategoryController.cs
@@ -31,6 +31,7 @@ public class CategoryController : Controller
         {
             _db.Categories.Add(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category created successfully!";
             return RedirectToAction("Index");
         }
 
@@ -61,6 +62,7 @@ public class CategoryController : Controller
         {
             _db.Categories.Update(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category updated successfully!";
             return RedirectToAction("Index");
         }
 
@@ -96,6 +98,7 @@ public class CategoryController : Controller
         _db.Categories.Remove(obj);
 
         _db.SaveChanges();
+        TempData["success"] = "Category deleted successfully!";
         return RedirectToAction("Index");
     }
 }
