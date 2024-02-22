405e4980e7019fbc490e325d84d0382eac9c9ef1
IgorSS <igorskor0912@gmail.com>
2024-02-22 20:31:42 +0200

    Replace CategoryRepository with UnitOfWork

--- Store.DataAccess/Repository/IRepository/ICategoryRepository.cs
+++ Store.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -5,5 +5,4 @@ namespace Store.DataAccess.Repository.IRepository;
 public interface ICategoryRepository : IRepository<Category>
 {
     void Update(Category obj);
-    void Save();
 }
--- Store/Program.cs
+++ Store/Program.cs
@@ -15,7 +15,7 @@ public class Program
         builder.Services.AddControllersWithViews();
         builder.Services.AddDbContext<ApplicationDbContext>(options=>
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-        builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+        builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
         var app = builder.Build();
 
