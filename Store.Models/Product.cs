bd72c8a4715c2d4d63b3d1b3e751b8ced63d1c83
IgorSS <igorskor0912@gmail.com>
2024-02-24 01:58:50 +0200

    Create model Product and seed data

---
+++ Store.Models/Product.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Store.Models;
+
+public class Product
+{
+    [Key] public int Id { get; set; }
+    [Required]
+    [MaxLength(50)]
+    [DisplayName("Product Title")]
+    public string Title { get; set; }
+    [Required]
+    public string Description { get; set; }
+    [Required]
+    public string Developer { get; set; }
+    [DisplayName("List Price")]
+    [Range(1, 1000)]
+    public double ListPrice { get; set; }
+    [DisplayName("Price for 1-3")]
+    [Range(1, 1000)]
+    public double Price { get; set; }
+    [DisplayName("Price for 4+")]
+    [Range(1, 1000)]
+    public double PriceFor4Copies { get; set; }
+}
