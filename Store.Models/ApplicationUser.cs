be23a15a5dde0aa18273006239f5fe104c6cde25
Ihor <igorskor0912@gmail.com>
2024-05-18 23:32:14 +0300

    Finish base identity

---
+++ Store.Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Store.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    [Required]
+    public string Name { get; set; }
+    
+    public string? StreetAddress { get; set; }
+    public string? City { get; set; }
+    public string? State { get; set; }
+    public string? PostalCode { get; set; }
+}
--- Store/Program.cs
+++ Store/Program.cs
@@ -16,8 +16,9 @@ public class Program
         builder.Services.AddControllersWithViews();
         builder.Services.AddDbContext<ApplicationDbContext>(options=>
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+        
         builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+        builder.Services.AddRazorPages();
         builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
         var app = builder.Build();
@@ -37,6 +38,7 @@ public class Program
         app.UseAuthentication();
         app.UseAuthorization();
 
+        app.MapRazorPages();
         app.MapControllerRoute(
             name: "default",
             pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
