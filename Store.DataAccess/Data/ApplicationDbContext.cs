9f37766f22e497ac11643a56a1a04642eb34bf02
Ihor <igorskor0912@gmail.com>
2024-05-24 23:04:09 +0300

    Create ShoppingCart model

--- Store.DataAccess/Data/ApplicationDbContext.cs
+++ Store.DataAccess/Data/ApplicationDbContext.cs
@@ -9,8 +9,11 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
 {
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
+
+    public DbSet<ShoppingCart> ShoppingCarts { get; set; }
     
     public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
