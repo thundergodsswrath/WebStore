efc92f96025372fef0dd8dea76fb48b1e24a0576
Ihor <igorskor0912@gmail.com>
2024-05-25 16:40:41 +0300

    Make entity not trackable

--- Store.DataAccess/Repository/IRepository/IRepository.cs
+++ Store.DataAccess/Repository/IRepository/IRepository.cs
@@ -5,7 +5,7 @@ namespace Store.DataAccess.Repository.IRepository;
 public interface IRepository<T> where T : class
 {
     IEnumerable<T> GetAll(string? includeProperties = null);
-    T Get(Expression<Func<T, bool>> filter, string? includeProperties = null);
+    T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false);
     void Create(T entity);
     void Remove(T entity);
     void RemoveRange(IEnumerable<T> entities);
