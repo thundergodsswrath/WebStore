a18c1bfbb3346de7a2617ecc13b1c2b951f3a3d7
Ihor <igorskor0912@gmail.com>
2024-05-25 16:48:07 +0300

    Create ShoppingCart logic

--- Store/Areas/Customer/Controllers/HomeController.cs
+++ Store/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using System.Diagnostics;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Store.DataAccess.Repository.IRepository;
 using Store.Models;
@@ -24,8 +26,38 @@ public class HomeController : Controller
     
     public IActionResult Details(int productId)
     {
-        Product product= _unitOfWork.ProductRepository.Get(u=>u.Id == productId, includeProperties:"Category");
-        return View(product);
+        ShoppingCart shoppingCart = new()
+        {
+            Product = _unitOfWork.ProductRepository.Get(u => u.Id == productId, includeProperties: "Category"),
+            Count = 1,
+            ProductId = productId
+        };
+        return View(shoppingCart);
+    }
+    
+    [HttpPost]
+    [Authorize]
+    public IActionResult Details(ShoppingCart shoppingCart)
+    {
+        var claimsIdentity = (ClaimsIdentity)User.Identity;
+        var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+        shoppingCart.ApplicationUserId = userId;
+
+        ShoppingCart cartFromDb = _unitOfWork.ShoppingCartRepository.Get(u => u.ApplicationUserId == userId &&
+                                                                              u.ProductId==shoppingCart.ProductId);
+        if (cartFromDb is not null)
+        {
+            cartFromDb.Count += shoppingCart.Count;
+            _unitOfWork.ShoppingCartRepository.Update(cartFromDb);
+        }
+        else
+        {
+            _unitOfWork.ShoppingCartRepository.Create(shoppingCart);
+        }
+        
+        _unitOfWork.Save();
+        
+        return RedirectToAction(nameof(Index));
     }
 
     public IActionResult Privacy()
