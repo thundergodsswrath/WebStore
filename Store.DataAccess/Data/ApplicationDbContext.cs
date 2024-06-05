87e61a8bcd9e93ddcad337d9196329a1116b640c
Ihor <igorskor0912@gmail.com>
2024-06-05 13:25:13 +0300

    Add OrderHeader and orderDetail to db

--- Store.DataAccess/Data/ApplicationDbContext.cs
+++ Store.DataAccess/Data/ApplicationDbContext.cs
@@ -14,6 +14,9 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     
     public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     
+    public DbSet<OrderHeader> OrderHeaders { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
+    
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
