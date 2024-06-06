d5accf8a37a6e5167edf9b1ba7251383950d60d5
Ihor <igorskor0912@gmail.com>
2024-06-06 13:01:06 +0300

    Make Update order details logic

--- Store/Areas/Admin/Controllers/OrderController.cs
+++ Store/Areas/Admin/Controllers/OrderController.cs
@@ -13,44 +13,81 @@ namespace Store.Areas.Admin.Controllers;
 public class OrderController : Controller
 {
     private readonly IUnitOfWork _unitOfWork;
+    [BindProperty]
     public OrderVM OrderVM { get; set; }
-    
+
     public OrderController(IUnitOfWork unitOfWork)
     {
         _unitOfWork = unitOfWork;
     }
-    
+
     public IActionResult Index()
     {
         return View();
     }
-    
-    public IActionResult Details(int orderId) {
-        OrderVM = new() {
-            OrderHeader = _unitOfWork.OrderHeaderRepository.Get(u => u.Id == orderId, includeProperties: "ApplicationUser"),
-            OrderDetail = _unitOfWork.OrderDetailRepository.GetAll(u => u.OrderHeaderId == orderId, includeProperties: "Product")
+
+    public IActionResult Details(int orderId)
+    {
+        OrderVM = new()
+        {
+            OrderHeader =
+                _unitOfWork.OrderHeaderRepository.Get(u => u.Id == orderId, includeProperties: "ApplicationUser"),
+            OrderDetail =
+                _unitOfWork.OrderDetailRepository.GetAll(u => u.OrderHeaderId == orderId, includeProperties: "Product")
         };
 
         return View(OrderVM);
     }
 
+    [HttpPost]
+    [Authorize(Roles = StaticDetails.RoleAdmin + "," + StaticDetails.RoleEmployee)]
+    public IActionResult UpdateOrderDetail()
+    {
+        var orderHeaderFromDb = _unitOfWork.OrderHeaderRepository.Get(u => u.Id == OrderVM.OrderHeader.Id);
+        orderHeaderFromDb.Name = OrderVM.OrderHeader.Name;
+        orderHeaderFromDb.PhoneNumber = OrderVM.OrderHeader.PhoneNumber;
+        orderHeaderFromDb.StreetAddress = OrderVM.OrderHeader.StreetAddress;
+        orderHeaderFromDb.City = OrderVM.OrderHeader.City;
+        orderHeaderFromDb.State = OrderVM.OrderHeader.State;
+        orderHeaderFromDb.PostalCode = OrderVM.OrderHeader.PostalCode;
+        if (!string.IsNullOrEmpty(OrderVM.OrderHeader.Carrier))
+        {
+            orderHeaderFromDb.Carrier = OrderVM.OrderHeader.Carrier;
+        }
+
+        if (!string.IsNullOrEmpty(OrderVM.OrderHeader.TrackingNumber))
+        {
+            orderHeaderFromDb.Carrier = OrderVM.OrderHeader.TrackingNumber;
+        }
+
+        _unitOfWork.OrderHeaderRepository.Update(orderHeaderFromDb);
+        _unitOfWork.Save();
+
+
+        return RedirectToAction(nameof(Details), new { orderId = orderHeaderFromDb.Id });
+    }
+
     [HttpGet]
     public IActionResult GetAll(string status)
     {
-        IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeaderRepository.GetAll(includeProperties: "ApplicationUser").ToList();
-        
-        if(User.IsInRole(StaticDetails.RoleAdmin)|| User.IsInRole(StaticDetails.RoleEmployee)) {
+        IEnumerable<OrderHeader> objOrderHeaders =
+            _unitOfWork.OrderHeaderRepository.GetAll(includeProperties: "ApplicationUser").ToList();
+
+        if (User.IsInRole(StaticDetails.RoleAdmin) || User.IsInRole(StaticDetails.RoleEmployee))
+        {
             objOrderHeaders = _unitOfWork.OrderHeaderRepository.GetAll(includeProperties: "ApplicationUser").ToList();
         }
-        else {
+        else
+        {
             var claimsIdentity = (ClaimsIdentity)User.Identity;
             var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
 
             objOrderHeaders = _unitOfWork.OrderHeaderRepository
                 .GetAll(u => u.ApplicationUserId == userId, includeProperties: "ApplicationUser");
         }
-        
-        switch (status) {
+
+        switch (status)
+        {
             case "pending":
                 objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == StaticDetails.PaymentStatusPending);
                 break;
@@ -66,7 +103,7 @@ public class OrderController : Controller
             default:
                 break;
         }
-        
+
         return Json(new { data = objOrderHeaders });
     }
 }
