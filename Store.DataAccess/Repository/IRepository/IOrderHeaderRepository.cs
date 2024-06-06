8592515ece9ddbabe6c6e54449cdff5f882764ed
Ihor <igorskor0912@gmail.com>
2024-06-06 04:33:25 +0300

    Add sessionId to the OrderHeader model and add UpdateStatus, UpdateStripePaymentId methods to the OrderHeader Repository

--- Store.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
+++ Store.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
@@ -5,4 +5,6 @@ namespace Store.DataAccess.Repository.IRepository;
 public interface IOrderHeaderRepository : IRepository<OrderHeader>
 {
     void Update(OrderHeader obj);
+    void UpdateStatus(int id, string orderStatus, string? paymentStatus = null);
+    void UpdateStripePaymentId(int id, string sessionId, string paymentIntentId);
 }
--- Store.DataAccess/Repository/OrderHeaderRepository.cs
+++ Store.DataAccess/Repository/OrderHeaderRepository.cs
@@ -16,4 +16,31 @@ public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderReposi
     {
         _db.OrderHeaders.Update(obj);
     }
+
+    public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+    {
+        var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
+        if (orderFromDb is not null)
+        {
+            orderFromDb.OrderStatus = orderStatus;
+            if (!string.IsNullOrEmpty(paymentStatus))
+            {
+                orderFromDb.PaymentStatus = paymentStatus;
+            }
+        }
+    }
+
+    public void UpdateStripePaymentId(int id, string sessionId, string paymentIntentId)
+    {
+        var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
+        if (!string.IsNullOrEmpty(sessionId))
+        {
+            orderFromDb.SessionId = sessionId;
+        }
+        if (!string.IsNullOrEmpty(paymentIntentId))
+        {
+            orderFromDb.PaymentIntentId = paymentIntentId;
+            orderFromDb.PaymentDate = DateTime.Now;
+        }
+    }
 }
