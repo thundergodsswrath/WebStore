21dded70c26f1bbfbed88e7210120a3a0c8936cc
Ihor <igorskor0912@gmail.com>
2024-06-06 04:59:55 +0300

    Create stripe payment logic

--- Store/Areas/Customer/Controllers/CartController.cs
+++ Store/Areas/Customer/Controllers/CartController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 using Store.DataAccess.Repository.IRepository;
 using Store.Models;
 using Store.Models.ViewModels;
+using Store.Utility;
+using Stripe.Checkout;
 
 namespace Store.Areas.Customer.Controllers;
 
@@ -12,6 +14,7 @@ namespace Store.Areas.Customer.Controllers;
 public class CartController : Controller
 {
     private readonly IUnitOfWork _unitOfWork;
+    [BindProperty]
     public ShoppingCartVM ShoppingCartVM { get; set; }
 
     public CartController(IUnitOfWork unitOfWork)
@@ -103,6 +106,88 @@ public class CartController : Controller
         }
 
         return View(ShoppingCartVM);
+    }
+    
+    [HttpPost]
+    [ActionName("Summary")]
+    public IActionResult SummaryPOST()
+    {
+        var claimsIdentity = (ClaimsIdentity)User.Identity;
+        var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+        ShoppingCartVM.ShoppingCartList = _unitOfWork.ShoppingCartRepository.GetAll(u => u.ApplicationUserId == userId,
+                includeProperties: "Product");
+
+        ShoppingCartVM.OrderHeader.OrderDate = DateTime.Now;
+        ShoppingCartVM.OrderHeader.ApplicationUserId = userId;
+
+        ApplicationUser applicationUser = _unitOfWork.ApplicationUserRepository.Get(u => u.Id == userId);
+
+        foreach (var cart in ShoppingCartVM.ShoppingCartList)
+        {
+            cart.Price = GetPriceBasedOnQuantity(cart);
+            ShoppingCartVM.OrderHeader.OrderTotal += cart.Price * cart.Count;
+        }
+
+        ShoppingCartVM.OrderHeader.OrderStatus = StaticDetails.StatusPending;
+        ShoppingCartVM.OrderHeader.PaymentStatus = StaticDetails.PaymentStatusPending;
+        
+        _unitOfWork.OrderHeaderRepository.Create(ShoppingCartVM.OrderHeader);
+        _unitOfWork.Save();
+
+        foreach (var cart in ShoppingCartVM.ShoppingCartList)
+        {
+            OrderDetail orderDetail = new()
+            {
+                ProductId = cart.ProductId,
+                OrderHeaderId = ShoppingCartVM.OrderHeader.Id,
+                Price = cart.Price,
+                Count = cart.Count
+            };
+            _unitOfWork.OrderDetailRepository.Create(orderDetail);
+            _unitOfWork.Save();
+        }
+
+        var domain = "http://localhost:5264/";
+        
+        var options = new Stripe.Checkout.SessionCreateOptions
+        {
+            SuccessUrl = domain + $"Customer/Cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
+            CancelUrl = domain + $"Customer/Cart/Index",
+            LineItems = new List<Stripe.Checkout.SessionLineItemOptions>(),
+            Mode = "payment",
+        };
+
+        foreach (var item in ShoppingCartVM.ShoppingCartList)
+        {
+            var sessionLineItem = new SessionLineItemOptions
+            {
+                PriceData = new SessionLineItemPriceDataOptions
+                {
+                    UnitAmount = (long)(item.Price * 100),
+                    Currency = "usd",
+                    ProductData = new SessionLineItemPriceDataProductDataOptions
+                    {
+                        Name = item.Product.Title
+                    }
+                },
+                Quantity = item.Count
+            };
+            options.LineItems.Add(sessionLineItem);
+        }
+        
+        var service = new Stripe.Checkout.SessionService();
+        Session session = service.Create(options);
+        _unitOfWork.OrderHeaderRepository.UpdateStripePaymentId(ShoppingCartVM.OrderHeader.Id, session.Id, session.PaymentIntentId);
+        _unitOfWork.Save();
+        
+        Response.Headers.Add("Location", session.Url);
+        return new StatusCodeResult(303);
+    }
+
+    public IActionResult OrderConfirmation(int id)
+    {
+        return View(id);
     }
 
     private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
