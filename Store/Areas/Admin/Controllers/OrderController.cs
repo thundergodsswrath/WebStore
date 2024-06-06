ba7c9efc40321d2bbc5c04f0ca9693f26b336891
Ihor <igorskor0912@gmail.com>
2024-06-06 12:31:06 +0300

    Create filters in Order Management

--- Store/Areas/Admin/Controllers/OrderController.cs
+++ Store/Areas/Admin/Controllers/OrderController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Store.DataAccess.Repository.IRepository;
 using Store.Models;
+using Store.Utility;
 
 namespace Store.Areas.Admin.Controllers;
 
@@ -25,6 +26,24 @@ public class OrderController : Controller
     public IActionResult GetAll(string status)
     {
         IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeaderRepository.GetAll(includeProperties: "ApplicationUser").ToList();
+        
+        switch (status) {
+            case "pending":
+                objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == StaticDetails.PaymentStatusPending);
+                break;
+            case "inprocess":
+                objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusInProcess);
+                break;
+            case "completed":
+                objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusShipped);
+                break;
+            case "approved":
+                objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusApproved);
+                break;
+            default:
+                break;
+        }
+        
         return Json(new { data = objOrderHeaders });
     }
 }
--- Store/wwwroot/js/order.js
+++ Store/wwwroot/js/order.js
@@ -1,7 +1,28 @@
 var dataTable;
 
 $(document).ready(function () {
-    loadDataTable();
+    var url = window.location.search;
+    if (url.includes("inprocess")) {
+        loadDataTable("inprocess");
+    }
+    else {
+        if (url.includes("completed")) {
+            loadDataTable("completed");
+        }
+        else {
+            if (url.includes("pending")) {
+                loadDataTable("pending");
+            }
+            else {
+                if (url.includes("approved")) {
+                    loadDataTable("approved");
+                }
+                else {
+                    loadDataTable("all");
+                }
+            }
+        }
+    }
 });
 
 function loadDataTable(status) {
