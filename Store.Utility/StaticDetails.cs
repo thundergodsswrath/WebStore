4e8c49d2b0f4a29275029ef6ae24e16a4f42c75e
Ihor <igorskor0912@gmail.com>
2024-06-06 03:46:24 +0300

    Add statuses for the order and payment

--- Store.Utility/StaticDetails.cs
+++ Store.Utility/StaticDetails.cs
@@ -5,4 +5,15 @@ public static class StaticDetails
     public const string RoleCustomer = "Customer";
     public const string RoleAdmin = "Admin";
     public const string RoleEmployee = "Employee";
+
+    public const string StatusPending = "Pending";
+    public const string StatusApproved = "Approved";
+    public const string StatusInProcess = "Processing";
+    public const string StatusShipped = "Shipped";
+    public const string StatusCanceled = "Canceled";
+    public const string StatusRefunded = "Refunded";
+
+    public const string PaymentStatusPending = "Pending";
+    public const string PaymentStatusApproved = "Approved";
+    public const string PaymentStatusRejected = "Rejected";
 }
